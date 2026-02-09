2443c34439c7214b121b7c197d8df3d6d1b43118
Luis Adrian <luisadriannu13@gmail.com>
2026-02-09 15:55:39 -0600

    cambio de foto

--- src/data/birthday/abigail-21.ts
+++ src/data/birthday/abigail-21.ts
@@ -15,7 +15,7 @@ const event: EventData = {
     "https://images.pexels.com/photos/2114365/pexels-photo-2114365.jpeg?auto=compress&cs=tinysrgb&w=500",
     "https://images.pexels.com/photos/36068378/pexels-photo-36068378.jpeg?auto=compress&cs=tinysrgb&w=500",
     "https://images.pexels.com/photos/18604369/pexels-photo-18604369.jpeg?auto=compress&cs=tinysrgb&w=500",
-    "https://images.pexels.com/photos/30870982/pexels-photo-30870982.jpeg?auto=compress&cs=tinysrgb&w=500",
+    "https://images.pexels.com/photos/33409270/pexels-photo-33409270.jpeg?auto=compress&cs=tinysrgb&w=500",
   ],
   location:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6317.156972930946!2d-101.1756539283774!3d19.70393671011761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842d0e0d2a8351c9%3A0x648455118dfa41fc!2sFuente%20de%20las%20Tarascas!5e0!3m2!1ses!2smx!4v1769631855925!5m2!1ses!2smx",
