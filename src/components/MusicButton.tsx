58be58a8aa5594d625793d27c5646e1ccc552ede
Luis Adrian <luisadriannu13@gmail.com>
2026-02-23 13:40:56 -0600

    Invitacion jose y asminda

--- src/components/MusicButton.tsx
+++ src/components/MusicButton.tsx
@@ -34,7 +34,7 @@ export default function MusicButton({ src }: Props) {
     <button
       onClick={toggleMusic}
       className="
-        fixed bottom-6 right-6 z-50
+        fixed bottom-6 right-6 z-[60]
         w-10 h-10 rounded-full
         flex items-center justify-center
         bg-black/70 backdrop-blur
