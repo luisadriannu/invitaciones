bc226683338092fa952398602551139157cd8b08
Luis Adrian <luisadriannu13@gmail.com>
2026-03-31 09:01:17 -0600

    Removiendo invitacion

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,10 +1,8 @@
 import type { EventData } from "@/types/EventData";
 import geovannyyyuritzi from "@/data/weddings/geovanny-y-yuritzi";
-import joseyasminda from "@/data/weddings/jose-y-asminda";
 
 const events: Record<string, EventData> = {
   "geovanny-y-yuritzi": geovannyyyuritzi,
-  "jose-y-asminda": joseyasminda,
   "yuritzi-y-geovanny": geovannyyyuritzi,
 };
 
