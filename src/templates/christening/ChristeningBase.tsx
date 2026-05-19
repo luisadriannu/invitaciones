302f232f7418293218a625e83b2845fd4b6ecb93
Luis Adrian <luisadriannu13@gmail.com>
2026-05-19 15:44:35 -0600

    Removiendo bug

--- src/templates/christening/ChristeningBase.tsx
+++ src/templates/christening/ChristeningBase.tsx
@@ -115,21 +115,21 @@ Confirmo mi asistencia al bautizo.
   //   "Sábado",
   // ];
 
-  const meses: Record<string, string> = {
-    Enero: "01",
-    Febrero: "02",
-    Marzo: "03",
-    Abril: "04",
-    Mayo: "05",
-    Junio: "06",
-    Julio: "07",
-    Agosto: "08",
-    Septiembre: "09",
-    Octubre: "10",
-    Noviembre: "11",
-    Diciembre: "12",
-  };
-  const [dia, mes] = data.date.split(" ");
+  // const meses: Record<string, string> = {
+  //   Enero: "01",
+  //   Febrero: "02",
+  //   Marzo: "03",
+  //   Abril: "04",
+  //   Mayo: "05",
+  //   Junio: "06",
+  //   Julio: "07",
+  //   Agosto: "08",
+  //   Septiembre: "09",
+  //   Octubre: "10",
+  //   Noviembre: "11",
+  //   Diciembre: "12",
+  // };
+  // const [dia, mes] = data.date.split(" ");
 
   const [
     dayNumberStr,
